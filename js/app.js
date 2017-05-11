fd6e952d5fdb643a52a99ea146bfe9687a886c18
Julia Kim <jkim0620@gmail.com>
2017-05-11 12:05:32 -0400

    added a function for card shuffle

--- js/app.js
+++ js/app.js
@@ -22,18 +22,27 @@ $(document).ready(function() {
       #4-2. if user wants to end, end the game.
   END
   */
+
+  // variables
   let $startBtn = $("#startBtn");
+  let $endBtn = $("#endBtn");
+  let $okBtn = $("#okBtn");
+  let $goBtn = $("#goBtn");
   let $exit = $("#exit");
 
   let $card = $(".card");
   let cardArr = [];
+
+  let randomCard;
 
   // Creating an array of cards
   for (let i = 0; i < $card.length; i++) {
     cardArr.push($card[i]);
   }
 
-  // Shuffle cards function
+  // Function for shuffling cards
+  // Couldn't figure out how to shuffle an array so got some help from stackoverflow
+  // (http://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array)
   let shuffleCards = function(array) {
     for (let i = array.length - 1; i > 0; i--) {
       let j = Math.floor(Math.random() * (i + 1));
@@ -44,11 +53,44 @@ $(document).ready(function() {
     return array;
   }
 
-
   console.log(cardArr);
 
-  $("#tempBtn").on("click", function() {
-    console.log("clicked");
+  let pickRandomCard = function(arr) {
+    let i = Math.floor(Math.random() * arr.length);
+    return arr[i];
+    // return array[i];
+  }
+
+  // Testing shuffleCards function on click
+  // $("#tempBtn").on("click", function() {
+  //   console.log("clicked");
+  //   $(".card").remove();
+  //   shuffleCards(cardArr);
+  //   console.log(cardArr);
+  //   console.log(cardArr[0]);
+  //   for(let i = 0; i < cardArr.length; i++) {
+  //     $(".container").append(cardArr[i]);
+  //   }
+  // });
+
+  $startBtn.on("click", function() {
+    console.log('click');
+    $(".modal-intro-wrapper").fadeOut(500);
+    $(".modal-instruction-wrapper").fadeIn(500);
+  });
+
+  $okBtn.on("click", function() {
+    console.log('click');
+    $(".modal-instruction-wrapper").fadeOut(500);
+    $(".modal-popup-wrapper").fadeIn(500);
+    let findMe = pickRandomCard(cardArr);
+    $(".card-box").append(findMe);
+    console.log(pickRandomCard(cardArr));
+  });
+
+  $goBtn.on("click", function() {
+    console.log('click');
+    $(".modal-popup-wrapper").fadeOut(500);
     $(".card").remove();
     shuffleCards(cardArr);
     console.log(cardArr);
@@ -58,14 +100,10 @@ $(document).ready(function() {
     }
   });
 
-  $startBtn.on("click", function() {
-    console.log('click');
-    $(".modal-wrapper").fadeOut(500);
-  });
-
   $exit.on("click", function() {
     $(".modal-wrapper-2").fadeOut(500);
   });
+
 
 
 });
